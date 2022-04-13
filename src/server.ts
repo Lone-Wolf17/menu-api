9679d024939ebf9155f2d8e638c185b9ae2f93e9
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2022-04-13 18:39:34 +0100

    ** set up item model, service, routes & controller

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 
+import {itemsRouter} from './items/items.router';
+
 dotenv.config();
 
 /**
@@ -27,6 +29,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
 
 /**
  * Server Activation
